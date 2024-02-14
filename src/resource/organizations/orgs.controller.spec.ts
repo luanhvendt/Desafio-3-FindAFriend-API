b49d3664c62fcf1c02036adaf6c024d6eb651d44
Luan Vendt <luan.vendt@bitize.com.br>
2024-02-14 10:31:57 -0300

    Teste get/id e2e organização

--- src/resource/organizations/orgs.controller.spec.ts
+++ src/resource/organizations/orgs.controller.spec.ts
@@ -157,6 +157,39 @@ describe('OrgsController', () => {
         expect(response.statusCode).toBe(200)
     })
 
+    test('[GET] /organization/:id', async () => {
+        const org = await prisma.organization.create({
+            data: {
+                name: 'organization',
+                email: `teste@${randomUUID()}@mail.com`,
+                cep: '18016040',
+                adress: 'adress',
+                whatsapp: '123456789',
+                password: '1234abcd'
+            }
+        })
+
+        const responseLogin = await request(app.getHttpServer())
+            .post('/auth/login')
+            .send(org)
+
+        expect(responseLogin.statusCode).toBe(200)
+
+        const responseBody = JSON.parse(responseLogin.text)
+        const accessToken = responseBody.accesToken
+        testAccessToken = accessToken
+
+        const refreshToken = responseBody.refreshToken
+        testRefreshToken = refreshToken
+
+        const response = await request(app.getHttpServer())
+            .get(`/organization/${org.id}`)
+            .set('Authorization', `Bearer ${accessToken}`)
+            .send()
+
+        expect(response.statusCode).toBe(200)
+    })
+
     test('[DELETE] /organization/:id', async () => {
         const org = await prisma.organization.create({
             data: {
